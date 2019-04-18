d0007b3017e94b8cb7fa000d502dd9a54546755b
ChaosCat <shady3300@outlook.com>
2019-04-18 19:29:52 +0000

    add queue definitions

--- bones/queue.h
+++ bones/queue.h
@@ -1,12 +1,20 @@
 #ifndef BONES_QUEUE_H
 #define BONES_QUEUE_H
 
-typedef struct queue {
-    
-} queue_t;
+#include <stdint.h>
+#include "signal.h"
+
+#define QUEUE_SIZE 16
 
 typedef queue_node {
-    
+    signal_id_t         id;
+    void*               data;
+    signal_callback_f_t callback;
 } queue_node_t;
+
+typedef struct queue {
+    // TODO add dynamic size for profiling member
+    signal_node_t signals[QUEUE_SIZE];
+} queue_t;
 
 #endif
