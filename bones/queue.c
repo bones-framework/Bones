dbe1757f7ac301409c471518d7f63b81945459f4
ChaosCat <shady3300@outlook.com>
2019-04-19 13:25:41 +0000

    add: generic queue implementation and refactoring

--- bones/queue.c
+++ bones/queue.c
@@ -1,55 +1,41 @@
-#ifndef BONES_QUEUE_C
-#define BONES_QUEUE_C
-
 #include "queue.h"
 
-error_t push_signal(queue_ptr_t queue, signal_wrap_ptr_t signal_wrap) {
-    register queue_index_t queue_head = queue->head;
-    register signal_wrap_ptr sig;
+error_t enqueue(queue_t* queue, void* element) {
+
+    CHECK_ERROR(queue->state == QUEUE_FULL,
+		    ERROR_QUEUE_FULL);
+
+
+    memcpy(&(queue->elements[queue->head *
+		queue->element_size]),
+		element,
+		queue->element_size
+            );
+
     
-    if (queue->state == QUEUE_FULL) {
-        return ERROR_QUEUE_FULL;
-    }
-
-    (queue_head >= QUEUE_SIZE - 1) ? queue->head = 0 : \
-                                     queue->head++;
-
-    sig = &(queue->signals[queue_head]);
-    sig->id = wrap_signal->id;
-    sig->data = wrap_signal->data;
-    sig->callback = wrap_signal_callback;
-
-    if (queue->head == queue->tail) {
-        queue->state = QUEUE_FULL;
-    }
-
+    (queue->head < QUEUE_SIZE - 1) ? queue->head++ : \
+	                             queue->head = 0;
+    
+    queue->state = (queue->head == queue->tail) ? \
+		   QUEUE_FULL : QUEUE_AVAIL;
     return NO_ERROR;
 }
 
-error_t pop_signal(queue_ptr_t queue, signal_wrap_ptr_t signal_wrap_dest) {
-    register queue_index_t queue_tail;
-    register signal_wrap_ptr sig;
+error_t dequeue(queue_t* queue, void* element_dest) {
+    
+    CHECK_ERROR(queue->state == QUEUE_EMPTY,
+		    ERROR_QUEUE_EMPTY);
 
-    if (queue->state == QUEUE_EMPTY) {
-        return ERROR_QUEUE_EMPTY;
-    }
+    memcpy(element_dest,
+           &(queue->elements[queue->tail *
+		   queue->element_size]),
+	   queue->element_size);
 
-    (queue_tail >= QUEUE_SIZE - 1) ? queue->tail = 0 : \
-                                     queue->tail++;
+    (queue->tail < QUEUE_SIZE - 1) ? queue->tail++ : \
+	                             queue->tail = 0;
 
-    sig = &(queue->signals[queue_tail]);
-    signal_wrap_dest->id = sig->id;
-    signal_wrap_dest->data = sig->data;
-    signal_wrap_dest->callback = sig->callback;
-
-    // Shred
-    sig->id = 0;
-    sig->data = NULL;
-    sig->callback = NULL;
-
-    if (queue->tail == queue->head) {
-        queue->state = QUEUE_EMPTY;
-    }
+    queue->state = (queue->tail == queue->head) ? \
+		   QUEUE_EMPTY : QUEUE_AVAIL;
+    return NO_ERROR;
 }
 
-#endif
--- bones/queue.h
+++ bones/queue.h
@@ -5,31 +5,31 @@
 #include "signal.h"
 #include "errors.h"
 
-#define QUEUE_SIZE 16
+#define QUEUE_SIZE 512
 
 typedef signal_wrap {
     signal_id_t         id;
     void*               data;
     signal_callback_f_t callback;
 } signal_wrap_t;
-typedef signal_wrap_t* signal_wrap_ptr_t;
 
-typedef size_t queue_index_t;
 typedef enum queue_state {
     QUEUE_EMPTY = 0,
     QUEUE_FULL  = 1,
+    QUEUE_AVAIL = 2,
 } queue_state_t;
 
-typedef struct queue {
-    // TODO add dynamic size for profiling member
-    queue_index_t head;
-    queue_index_t tail;
-    queue_state_t status;
-    signal_wrap_t signals[QUEUE_SIZE];
-} queue_t;
-typedef queue_t* queue_ptr_t;
+// TODO add dynamic queue size member for profiling
 
-error_t push_signal(queue_ptr_t queue, signal_wrap_ptr_t sig_wrap);
-error_t pop_signal(queue_ptr_t queue, signal_wrap_ptr_t sig_wrap_dest);
+typedef struct queue {
+    size_t head;
+    size_t tail;
+    size_t queue_state;
+    size_t element_size;
+    void*  elements;
+} queue_t;
+
+error_t enqueue(queue_t* queue, void* elem);
+error_t dequeue(queue_t* queue, void* elem_dest);
 
 #endif
