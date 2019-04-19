2fc6fea3ae4d45c81555892a99961e511824709e
ChaosCat <shady3300@outlook.com>
2019-04-19 14:12:52 +0000

    bugfix: refactor and fix syntax errors in conditionals

--- bones/queue.c
+++ bones/queue.c
@@ -14,8 +14,11 @@ error_t enqueue(queue_t* queue, void* element) {
             );
 
     
-    (queue->head < QUEUE_SIZE - 1) ? queue->head++ : 
-	                             queue->head = 0;
+    if (queue->head < QUEUE_SIZE - 1) {
+        queue->head++;
+    } else {
+	queue->head = 0;
+    }
     
     queue->state = (queue->head == queue->tail) ? \
 		   QUEUE_FULL : QUEUE_AVAIL;
@@ -35,8 +38,11 @@ error_t dequeue(queue_t* queue, void* element_dest) {
 		   queue->element_size]),
 	   queue->element_size);
 
-    (queue->tail < QUEUE_SIZE - 1) ? queue->tail++ : \
-	                             queue->tail = 0;
+    if (queue->tail < QUEUE_SIZE - 1) {
+	    queue->tail++;
+    } else {
+	    queue->tail = 0;
+    }
 
     queue->state = (queue->tail == queue->head) ? \
 		   QUEUE_EMPTY : QUEUE_AVAIL;
